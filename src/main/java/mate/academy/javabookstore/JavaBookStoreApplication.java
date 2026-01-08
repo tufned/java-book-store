86f4708f08243619e717a5b9888073e1a6a710a6
Denys Funderat <funderatdenis@gmail.com>
2026-01-08 18:36:12 +0100

    spring boot setup + checkstyle added + github ci workflow added

---
+++ src/main/java/mate/academy/javabookstore/JavaBookStoreApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.javabookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBookStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBookStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mate/academy/javabookstore/JavaBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.javabookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
