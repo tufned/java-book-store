99c0d6c1b82f36fd3d39bb53c333fcb195e8a00f
Denys Funderat <funderatdenis@gmail.com>
2026-01-10 12:44:36 +0100

    BookController name upd

--- src/main/java/mate/academy/javabookstore/controller/BookController.java
+++ src/main/java/mate/academy/javabookstore/controller/BookController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/books")
-public class BookControllerImpl {
+public class BookController {
     private final BookService bookService;
 
     @GetMapping
