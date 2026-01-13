8c9114f37c823b49c8eac60ddb0a74d20da97336
Denys Funderat <funderatdenis@gmail.com>
2026-01-13 23:18:06 +0100

    CreateBookRequestDto validation improvements

--- src/main/java/mate/academy/javabookstore/dto/book/CreateBookRequestDto.java
+++ src/main/java/mate/academy/javabookstore/dto/book/CreateBookRequestDto.java
@@ -1,24 +1,25 @@
 package mate.academy.javabookstore.dto.book;
 
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import mate.academy.javabookstore.constants.BookConstants;
 
 public record CreateBookRequestDto(
-        @NotNull
+        @NotBlank
         @Size(min = BookConstants.MIN_TITLE_SIZE, max = BookConstants.MAX_TITLE_SIZE)
         String title,
-        @NotNull
+        @NotBlank
         @Size(min = BookConstants.MIN_AUTHOR_SIZE, max = BookConstants.MAX_AUTHOR_SIZE)
         String author,
-        @NotNull
+        @NotBlank
         @Pattern(regexp = BookConstants.ISBN_REGEX, message = BookConstants.ISBN_REGEX_ERROR_MSG)
         String isbn,
         @NotNull
-        @Min(0)
+        @Positive
         BigDecimal price,
         String description,
         String coverImage
