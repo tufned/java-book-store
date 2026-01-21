b49b827689873b2432a21794f676e01b65df80d0
Denys Funderat <funderatdenis@gmail.com>
2026-01-21 02:11:16 +0100

    user dtos docs added

--- src/main/java/mate/academy/javabookstore/dto/user/UserDto.java
+++ src/main/java/mate/academy/javabookstore/dto/user/UserDto.java
@@ -1,10 +1,17 @@
 package mate.academy.javabookstore.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import mate.academy.javabookstore.constants.user.UserDocs;
+
 public record UserDto(
         Long id,
         String email,
+        @Schema(description = UserDocs.NAME_DESC, example = UserDocs.FIRST_NAME_EX)
         String firstName,
+        @Schema(description = UserDocs.NAME_DESC, example = UserDocs.LAST_NAME_EX)
         String lastName,
+        @Schema(description = UserDocs.SHIPPING_ADDRESS_DESC,
+                example = UserDocs.SHIPPING_ADDRESS_EX)
         String shippingAddress
 ) {
 }
--- src/main/java/mate/academy/javabookstore/dto/user/UserRegistrationRequestDto.java
+++ src/main/java/mate/academy/javabookstore/dto/user/UserRegistrationRequestDto.java
@@ -1,10 +1,12 @@
 package mate.academy.javabookstore.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import mate.academy.javabookstore.constants.user.UserConstants;
+import mate.academy.javabookstore.constants.user.UserDocs;
 import mate.academy.javabookstore.validate.FieldMatch;
 
 @FieldMatch(first = "password", second = "repeatPassword", message = "Passwords don't match")
@@ -14,17 +16,23 @@ public record UserRegistrationRequestDto(
         String email,
 
         @Pattern(regexp = UserConstants.PASSWORD_REGEX)
+        @Schema(description = UserDocs.PASSWORD_DESC, example = UserDocs.PASSWORD_EX)
         String password,
 
         @Pattern(regexp = UserConstants.PASSWORD_REGEX)
+        @Schema(description = UserDocs.REPEAT_PASSWORD_DESC, example = UserDocs.PASSWORD_EX)
         String repeatPassword,
 
         @Size(min = UserConstants.MIN_NAME_SIZE, max = UserConstants.MAX_NAME_SIZE)
+        @Schema(description = UserDocs.NAME_DESC, example = UserDocs.FIRST_NAME_EX)
         String firstName,
 
         @Size(min = UserConstants.MIN_NAME_SIZE, max = UserConstants.MAX_NAME_SIZE)
+        @Schema(description = UserDocs.NAME_DESC, example = UserDocs.LAST_NAME_EX)
         String lastName,
 
+        @Schema(description = UserDocs.SHIPPING_ADDRESS_DESC,
+                example = UserDocs.SHIPPING_ADDRESS_EX)
         String shippingAddress
 ) {
 }
