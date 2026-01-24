4f438b81321d28844afb84ef0b4ce584c96185da
Denys Funderat <funderatdenis@gmail.com>
2026-01-24 22:39:14 +0100

    checkstyle fix

--- src/main/java/mate/academy/javabookstore/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/mate/academy/javabookstore/exception/CustomGlobalExceptionHandler.java
@@ -5,8 +5,6 @@ import lombok.RequiredArgsConstructor;
 import mate.academy.javabookstore.constants.AppConstants;
 import mate.academy.javabookstore.dto.response.ErrorResponseDto;
 import mate.academy.javabookstore.utils.ResponseEntityProvider;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
